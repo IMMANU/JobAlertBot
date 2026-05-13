f1a0eb0164a47c6ac05af42c9e49ba16bbf9ae63
Manish <56414703+IMMANU@users.noreply.github.com>
2026-05-13 15:36:30 +0530

    Add LinkedIn job scraper and Telegram notifier

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+BOT_TOKEN = "8965927548:AAEXQicdBCJDQA7iLl_80vsUgk1z8ALh5xA"
+CHAT_ID = "606044555"
+
+URL = "https://www.linkedin.com/jobs/search/?keywords=AWS%20OR%20AWS%20Cloud%20OR%20Cloud%20Engineer%20OR%20Platform%20Engineer%20OR%20CloudOps%20OR%20Site%20Reliability%20Engineer"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+jobs = soup.find_all("div", class_="base-search-card")
+
+for job in jobs[:5]:
+
+    title = job.find("h3").text.strip()
+
+    company = job.find("h4").text.strip()
+
+    link = job.find("a")["href"]
+
+    text = f"""
+🚀 New LinkedIn Job
+
+💼 {title}
+🏢 {company}
+
+🔗 {link}
+"""
+
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    requests.post(
+        telegram_url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
+
+print("Done")
